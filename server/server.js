922a59a92383d2e715d7d1d86b1c80962874ef84
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-02-15 15:35:47 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,23 +20,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 
-const port = 3001;
+const port = process.env.PORT || 3001
 
 /** Http GET Request */
 app.get("/", (req, res) => {
   res.status(201).json("Home GET Request");
 });
 
-app.get('/appointment', (req, res) => res.json({key: "value"}))
+// app.get('/appointment', (req, res) => res.json({key: "value"}))
 
 
 // app.post("/payment", async (request, response) => {
