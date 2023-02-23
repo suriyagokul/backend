36abcf6917fe980d33315c0093fa28b816627ab9
suriyagokul <124340052+suriyagokul@users.noreply.github.com>
2023-02-23 11:59:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ const app = express();
 
 app.use(express.json({limit: '50mb'}));
 app.use(cors({origin: '*'}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://mern-health-app.onrender.com"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+
 app.use(morgan("tiny"));
 app.disable("x-powered-by"); // less hackers know about our stack
 app.use(bodyParser.json());
